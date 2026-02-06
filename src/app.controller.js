0aa2c73bf61930dfbffe471d3ef8207566f216c2
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2026-02-06 21:56:12 +0200

    Update app.controller.js

--- src/app.controller.js
+++ src/app.controller.js
@@ -20,7 +20,7 @@ const bootstrap = async (app, express) => {
     checkConnectionDB();
 
     app.use(cors(corsOptions));
-    app.options('*', cors(corsOptions));
+  
 
     app.use(express.json());
 
